dc9ffb6da287bd96802570ad8dc60ac024472535
Tetiana <tany@gmail.com>
2026-05-08 19:14:28 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const ratingButtons = document.querySelectorAll(".rating-btn");
+const submitBtn = document.getElementById("submit-btn");
+const ratingCard = document.getElementById("rating-card");
+const thankYouCard = document.getElementById("thank-you-card");
+const selectedRatingText = document.getElementById("selected-rating");
+
+let selectedRating = null;
+ratingButtons.forEach((button) =>{
+	button.addEventListener("click", () => {
+		ratingButtons.forEach((btn) => {
+			btn.classList.remove("active");
+		});
+		button.classList.add("active");
+		selectedRating = button.textContent;
+	});
+});
+submitBtn.addEventListener("click", () => {
+	if(!selectedRating) return;
+
+	selectedRatingText.textContent = selectedRating;
+
+	ratingCard.classList.add("hidden");
+
+	thankYouCard.classList.remove("hidden");
+});
